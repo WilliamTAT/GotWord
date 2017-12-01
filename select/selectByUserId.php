1cb633fa6c8fa2cc5b57b705d8e67fd4bc56424d
ning <906978985@qq.com>
2017-12-01 20:33:34 +0800

    update

--- select/selectByUserId.php
+++ select/selectByUserId.php
@@ -22,7 +22,6 @@ function selectByUserId($user_id) {
     $mysqli_result = mysqli_query($conn, "select word_word from word_has_group 
             where word_group_id = 
     		any(select id from word_group where user_id = '$user_id');");
-//    $mysqli_result = $conn -> query("select * from word_group where word_group_id = '$user_id'");
     if ($mysqli_result) {
         $array = mysqli_fetch_array($mysqli_result, MYSQLI_ASSOC);
         $GLOBALS['response']['length'] = count($array);
